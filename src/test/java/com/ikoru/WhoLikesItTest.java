2f0e6ec651b25502b1291ded762ffaefadb7e984
iKoru <glassonyou@gmail.com>
2019-12-08 20:19:56 +0900

    passed wholikesIt

---
+++ src/test/java/com/ikoru/WhoLikesItTest.java
@@ -0,0 +1,18 @@
+package com.ikoru;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class WhoLikesItTest {
+
+    @Test
+    public void staticTest() {
+        assertEquals("no one likes this", WhoLikesIt.whoLikesIt());
+        assertEquals("Peter likes this", WhoLikesIt.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", WhoLikesIt.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", WhoLikesIt.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", WhoLikesIt.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+
+}
